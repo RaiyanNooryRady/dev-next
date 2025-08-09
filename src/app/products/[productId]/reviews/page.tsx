8f6ccb6faddf5453261db7216c29bdd8426f3b65
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-09 15:01:02 +0600

    dynamic nested routings

--- src/app/products/[productId]/reviews/page.tsx
+++ src/app/products/[productId]/reviews/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 export default async function ProductReviews({params}: {params: {productId: string}}) {
     const {productId} = await params;
 
-    return <div>Product Reviews for product {productId}</div>;
+    return <div>
+        
+        <h2>Product Reviews for product {productId}</h2>
+        <ul className="reviews-list">
+            <li>
+                <Link href={`/products/${productId}/reviews/1`}>Review 1</Link>
+                <Link href={`/products/${productId}/reviews/2`}>Review 2</Link>
+                <Link href={`/products/${productId}/reviews/3`}>Review 3</Link>
+            </li>
+        </ul>
+    </div>;
 }
