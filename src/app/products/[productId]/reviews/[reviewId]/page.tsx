670e5775ab989dd8b5a922ee8b4d0af3bc472d45
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-09 14:38:35 +0600

    nested dynamic routes

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
-export default async function ProductReview({params}: {params: {productId: string, reviewId: string}}) {
-    const {productId, reviewId} = await params;
+
+interface Params{
+    productId: string, reviewId: string
+}
+export default async function ProductReview({ params }: { params: Params }) {
+    const { productId, reviewId } = await params;
 
     return <div>Product Review {reviewId} for product {productId}</div>;
 }
