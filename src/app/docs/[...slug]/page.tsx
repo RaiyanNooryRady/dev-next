07ca6a6c2488360de55368b04f95a07b4c8bc2ae
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-10 10:21:54 +0600

    nested slugs

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,9 +1,17 @@
 export default async function DocsSlug({params}:{params: {slug: string[]}}){
     const {slug} = await params;
+    const slugLength = slug.length;
+    console.log(slugLength);
     return(
         <div>
             <h1>Docs page</h1>
             <p>Slug: {slug.join('/')}</p>
+             {
+                slugLength===3? <div>Viewing docs for feature: {slug[0]} and concept: {slug[1]} and subconcept: {slug[2]}</div>:
+                slugLength===2? <div>Viewing docs for feature: {slug[0]} and concept: {slug[1]}</div>:
+                slugLength===1? <div>Viewing docs for feature: {slug[0]} </div>:
+                null
+             }
         </div>
     )
 }
