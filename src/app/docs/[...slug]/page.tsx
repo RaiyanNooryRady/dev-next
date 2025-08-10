bd352a96e1cf87a01f11fd00ffbb9656b83e3fc5
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-10 10:23:29 +0600

    expression slug length

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,17 +1,32 @@
-export default async function DocsSlug({params}:{params: {slug: string[]}}){
-    const {slug} = await params;
+export default async function DocsSlug({ params }: { params: { slug: string[] } }) {
+    const { slug } = await params;
     const slugLength = slug.length;
     console.log(slugLength);
-    return(
+    return (
         <div>
             <h1>Docs page</h1>
             <p>Slug: {slug.join('/')}</p>
-             {
-                slugLength===3? <div>Viewing docs for feature: {slug[0]} and concept: {slug[1]} and subconcept: {slug[2]}</div>:
-                slugLength===2? <div>Viewing docs for feature: {slug[0]} and concept: {slug[1]}</div>:
-                slugLength===1? <div>Viewing docs for feature: {slug[0]} </div>:
-                null
-             }
+            {
+                slugLength === 3 ?
+                    <div>
+                        Viewing docs for feature: {slug[0]} and concept: {slug[1]} and subconcept: {slug[2]}
+                        <h2>
+                            Hello World
+                        </h2>
+                    </div> :
+                    slugLength === 2 ?
+                        <div>Viewing docs for feature: {slug[0]} and concept: {slug[1]}
+                            <h2>
+                                Hello World
+                            </h2>
+                        </div> :
+                        slugLength === 1 ? <div>Viewing docs for feature: {slug[0]}
+                            <h2>
+                                Hello World
+                            </h2>
+                        </div> :
+                            null
+            }
         </div>
     )
 }
