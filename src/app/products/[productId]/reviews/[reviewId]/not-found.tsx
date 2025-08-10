bf049255b1c8d1d50313b979651a6f02733fec87
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-10 12:40:53 +0600

    udpated

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 export default function ReviewNotFound() {
+    const pathname = usePathname(); 
+    const productId = pathname.split("/")[2];
+    const reviewId = pathname.split("/")[4];
+    console.log(productId, reviewId);
     return (
         <div>
-            <h1>404! Review Not Found</h1>
+            <h1>404! Review {reviewId} for product {productId} Not Found</h1>
         </div>
     )
 }
