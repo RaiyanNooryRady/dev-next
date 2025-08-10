d03d99f5b546932e03951512c31b67900b9ceec9
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-10 12:29:20 +0600

    review not found

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function ReviewNotFound() {
+    return (
+        <div>
+            <h1>404! Review Not Found</h1>
+        </div>
+    )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -6,7 +6,7 @@ interface Params{
 export default async function ProductReview({ params }: { params: Params }) {
     const { productId, reviewId } = await params;
 
-    if(parseInt(reviewId) > 1000){
+    if(parseInt(reviewId) > 1000 || parseInt(reviewId) < 1){
         notFound();
     }
     return <div>Product Review {reviewId} for product {productId}</div>;
