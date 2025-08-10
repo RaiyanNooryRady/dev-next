07ebdf7f9f0a9929f7ada989acdd18c1a9582a1a
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-10 12:56:27 +0600

    accessing private component from a public one

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import LibPrivate from "../_lib/page"
 export default function Dashboard(){
     return (
         <div>
             <h1>Dashboard</h1>
+            <p>This is a private dashboard</p>
+            <LibPrivate />
         </div>
     )
 }
