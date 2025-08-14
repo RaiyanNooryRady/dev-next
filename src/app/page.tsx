cfc4b1e973ee0421b32d935849be0beae07e5227
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-14 11:10:05 +0600

    shade cn button component install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <h1>Hello World</h1>
-    </>
+
+      <Button>Button</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="link">Button</Button>
+      <Button variant="destructive">Button</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="link">Button</Button>
+      <Button variant="destructive">Button</Button>
+      <Button variant="outline">Button</Button>
+    </div>
   );
 }
